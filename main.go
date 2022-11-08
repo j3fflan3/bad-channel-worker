e6257a4ab8c4b9265254bd842c007137a97d06f6
Jeff Lane <11069981+j3fflan3@users.noreply.github.com>
2022-11-08 14:48:27 -0800

    Added more tests

--- main.go
+++ main.go
@@ -6,8 +6,25 @@ import (
 	"sync"
 )
 
+const customerThreadPool int = 10
+const numOfCustomers int = 999
+
 func main() {
 	log.Println("compute should return 97, got ", compute())
+	var consumerGroup sync.WaitGroup
+	consumerGroup.Add(customerThreadPool)
+	custLen, _ := compute2(&consumerGroup)
+	log.Printf("compute2 should return %v, got %v\n", numOfCustomers, custLen)
+	// for cust := range custs {
+	// 	log.Printf("%v\n", cust)
+	// }
+	custLen2, _ := compute3()
+	log.Printf("compute3 should return %v, got %v\n", numOfCustomers, custLen2)
+	// for cust := range custs2 {
+	// 	log.Printf("%v\n", cust)
+	// }
+	custLen3, _ := compute4()
+	log.Printf("compute4 should return %v, got %v\n", numOfCustomers, custLen3)
 }
 
 func compute() int {
@@ -52,4 +69,116 @@ func compute() int {
 	// 	log.Println(item)
 	// }
 	return len(count)
+}
+
+func compute2(consumerGroup *sync.WaitGroup) (int, []int) {
+	custLock := struct {
+		Customers []int
+		Mu        sync.Mutex
+	}{}
+	customerChannel := make(chan int, 1000)
+	results := make(chan bool, numOfCustomers)
+	for i := 0; i < numOfCustomers; i++ {
+		customerChannel <- i
+	}
+	close(customerChannel)
+	for n := 0; n < customerThreadPool; n++ {
+		go func() {
+			defer consumerGroup.Done()
+			for cust := range customerChannel {
+				// for j := 0; j < 21; j++ {
+				// 	count := 0
+				// 	for count < 2 {
+				// 		count++
+				// 		needMorePass := true
+				// 		for needMorePass {
+				// 			needMorePass = false
+				// 			var shardgroup sync.WaitGroup
+				// 			shardgroup.Add(97)
+				// 			for parentShard0 := 0; parentShard0 < 97; parentShard0++ {
+				// 				go func(parentShard int) {
+				// 					defer shardgroup.Done()
+				// 					// doing nothing more than looping
+				// 				}(parentShard0)
+				// 			}
+				// 			shardgroup.Wait()
+				// 		}
+				// 	}
+				// }
+				//custLock.Mu.Lock()
+				c := cust
+				custLock.Customers = append(custLock.Customers, c)
+				//custLock.Mu.Unlock()
+				results <- true
+			}
+		}()
+	}
+	for i := 0; i < numOfCustomers; i++ {
+		<-results
+	}
+	consumerGroup.Wait()
+	return len(custLock.Customers), custLock.Customers
+}
+
+type Customers struct {
+	Customers []int
+	Mu        sync.Mutex
+}
+
+var ACustomers Customers
+var ACustomers2 Customers
+
+func computeWorkerNoLock(id int, jobs <-chan int, results chan<- bool) {
+	for job := range jobs {
+		//log.Printf("worker %v, job %v", id, job)
+		//ACustomers.Mu.Lock()
+		ACustomers.Customers = append(ACustomers.Customers, job)
+		//ACustomers.Mu.Unlock()
+		results <- true
+	}
+}
+func computeWorker(id int, jobs <-chan int, results chan<- bool) {
+	for job := range jobs {
+		//log.Printf("worker %v, job %v", id, job)
+		ACustomers2.Mu.Lock()
+		ACustomers2.Customers = append(ACustomers2.Customers, job)
+		ACustomers2.Mu.Unlock()
+		results <- true
+	}
+}
+
+func compute3() (int, []int) {
+	customerChannel := make(chan int, numOfCustomers)
+	results := make(chan bool, numOfCustomers)
+	for n := 0; n < customerThreadPool; n++ {
+		go computeWorkerNoLock(n, customerChannel, results)
+	}
+	for i := 0; i < numOfCustomers; i++ {
+		n := i
+		customerChannel <- n
+	}
+	close(customerChannel)
+
+	for j := 0; j < numOfCustomers; j++ {
+		<-results
+	}
+	return len(ACustomers.Customers), ACustomers.Customers
+}
+
+func compute4() (int, []int) {
+	customerChannel := make(chan int, numOfCustomers)
+	results := make(chan bool, numOfCustomers)
+	for n := 0; n < customerThreadPool; n++ {
+		go computeWorker(n, customerChannel, results)
+	}
+	for i := 0; i < numOfCustomers; i++ {
+		n := i
+		customerChannel <- n
+	}
+	close(customerChannel)
+
+	for j := 0; j < numOfCustomers; j++ {
+		<-results
+	}
+	return len(ACustomers2.Customers), ACustomers2.Customers
 }
